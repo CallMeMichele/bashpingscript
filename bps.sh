d7d7a6fb8d574292bb6e3be6d764b0773471bc25
CallMeMichele <40526477+CallMeMichele@users.noreply.github.com>
2021-01-08 11:38:36 +0100

    Aggiornato script inutile

--- bps.sh
+++ bps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Bash Ping Script. Please support me visiting my GitHub page https://github.com/DiegoDev-It/. Feel free to contact about bugs or 
+echo "Useless Bash Ping Script. Please don't support me visiting my GitHub page https://github.com/DiegoDev-It/. Feel free to contact about bugs or 
 requests at diwgo3sk@protonmail.com
 Your user is:" 
 whoami >> /path/to/output.txt #User that runs script
@@ -8,5 +8,5 @@ echo ---------------------------------------------------------------------------
 date >> /path/to/output.txt
 ping -c [seconds] [ip] >> /path/to/output.txt  #Change values in [ ]! #Ping command.
 echo ------------------------------------------------------------------------------------------------------------ >> /path/to/output.txt
-echo "Finished!"
+echo "Aborto finito!"
 kill $$
